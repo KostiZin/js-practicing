74094403cb41f193aeadf65da3c3bd153c24a628
KostiZin <pancostian@gmail.com>
2023-05-01 12:45:47 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,62 @@
+// let a = 5
+// let b = 4
+// c = a + b
+// console.log(c);
+
+// let name = "Kostia";
+// console.log(name.length)
+
+// 
+
+
+
+// function longestString() {
+//       let longest = "";
+    
+//       for (let i = 0; i < arguments.length; i++) {
+//         if (arguments[i].length > longest.length) {
+//           longest = arguments[i];
+//         }
+//       }
+    
+//       return longest;
+//     }
+//     console.log(longestString(`1`, `12`, `2`, `53535`));
+
+// const planets = ["Earth", "Mars", "Venus"];
+// planets.push("Jupiter");
+// planets.push("Saturn", "Uranus", "Neptune");
+
+// console.log(planets);
+
+// function createArrayOfNumbers(min, max) {
+//     const numbers = [];
+    
+//   numbers.push(min, max);
+   
+//     return numbers;
+//   }
+//   console.log(createArrayOfNumbers(8, 10));
+
+
+
+
+
+    
+    // const fruits = ["apple", "orange", "banana", "onion", "kiwi"];
+    
+    // for (let index = 0; index < fruits.length; index += 1) {
+    //   console.log(`${index}: ${fruits[index]}`);
+    // }
+
+total = 0
+  
+
+for (let i = 1; i <= 10; i += 2) {
+
+  console.log(i);
+  total = total + i;
+  console.log(`Total is ${total}`);
+}
+// console.log("Total is " + total);
+console.log(`Total is ${total}`);
