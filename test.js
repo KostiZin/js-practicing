bc4fa10992e56b1eb099737f17b94b1627bceeb0
KostiZin <pancostian@gmail.com>
2023-05-01 21:28:59 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,19 +6,17 @@
 // let name = "Kostia";
 // console.log(name.length)
 
-// 
-
-
+//
 
 // function longestString() {
 //       let longest = "";
-    
+
 //       for (let i = 0; i < arguments.length; i++) {
 //         if (arguments[i].length > longest.length) {
 //           longest = arguments[i];
 //         }
 //       }
-    
+
 //       return longest;
 //     }
 //     console.log(longestString(`1`, `12`, `2`, `53535`));
@@ -31,32 +29,117 @@
 
 // function createArrayOfNumbers(min, max) {
 //     const numbers = [];
-    
+
 //   numbers.push(min, max);
-   
+
 //     return numbers;
 //   }
 //   console.log(createArrayOfNumbers(8, 10));
 
+// const fruits = ["apple", "orange", "banana", "onion", "kiwi"];
 
+// for (let index = 0; index < fruits.length; index += 1) {
+//   console.log(`${index}: ${fruits[index]}`);
+// }
 
+// total = 0
 
+// for (let i = 1; i <= 10; i += 2) {
 
-    
-    // const fruits = ["apple", "orange", "banana", "onion", "kiwi"];
-    
-    // for (let index = 0; index < fruits.length; index += 1) {
-    //   console.log(`${index}: ${fruits[index]}`);
-    // }
+//   console.log(i);
+//   total = total + i;
+//   console.log(`Total is ${total}`);
+// }
 
-total = 0
-  
+// console.log(`Total is ${total}`);
 
-for (let i = 1; i <= 10; i += 2) {
+// let value = 3;
+// let numbers = [1, 2, 3, 4, 5];
 
-  console.log(i);
-  total = total + i;
-  console.log(`Total is ${total}`);
+// function filterArray(numbers, value) {
+// 	for (let i = 0; i > value; i += 1) {
+// 		if
+// 	}
+// }
+// console.log(numbers);
+// let array1 = [1, 2, 3];
+// let array2 = [2, 4];
+
+// function getCommonElements(array1, array2) {
+//   let includeBoth = [];
+//   let first = [];
+
+//   for (let i = 0; i <= array1.length; i += 1) {
+//     first = i;
+//   }
+
+//   for (let k = 0; k <= array2.length; k += 1) {
+//     console.log(k);
+//   }
+//   console.log(first);
+//   console.log(includeBoth);
+// }
+// const planets = ["Earth", "Mars", "Venus"];
+
+// for (const planet of planets) {
+// 	console.log(planet);
+// }
+// let order = [12, 85, 37, 4];
+
+// let total = 0;
+
+// for (let i of order) {
+// 	total += i;
+// }
+
+// console.log(total);
+
+// let start = 3;
+// let end = 11;
+
+// let newNumbers = [];
+
+// for (let i = start; i <= end; i += 1) {
+// 	if (i % 2 === 0) {
+// 		newNumbers.push(i);
+// 	}
+// }
+// console.log(newNumbers);
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+// 	if (i % 5 === 0) {
+// 		number = i;
+// 		break;
+// 	}
+// }
+// console.log(number);
+
+// let start = 2;
+// let end = 6;
+// let divisor = 5;
+
+// // let number;
+
+// for (let i = start; i < end; i += 1) {
+// 	if (i % divisor === 0) {
+// 	}
+// 	retunrconsole.log(i);
+// }
+
+// 1. var
+let array = [1, 2, 3, 4, 5];
+let value = 3;
+// let includedNumber = includedNumber.b;
+// 2. cycle
+for (const i of array) {
+	// includedNumber = i;
+	// 3. if includes
+	if (value === i) {
+		console.log(true);
+	}
 }
-// console.log("Total is " + total);
-console.log(`Total is ${total}`);
+console.log(i);
