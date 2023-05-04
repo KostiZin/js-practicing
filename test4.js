0526bd35fa5b34e40b7cbf56d4b8a1cba55b855e
KostiZin <pancostian@gmail.com>
2023-05-03 22:45:06 -0400

    Update test4.js

--- test4.js
+++ test4.js
@@ -116,3 +116,29 @@
 // cards.splice(3, 1, "updated card-4");
 
 // console.log(cards);
+
+
+// OBJECTS
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4.7,
+//     price: 5000,
+//     tags: ["premium", "promoted", "top", "trusted"],
+//     owner: {
+//       name: "Henry Sibola",
+//       phone: "982-126-1588",
+//       email: "henry.carter@aptmail.com",
+//     },
+//   };
+  
+  
+//   apartment.area = 60;
+//   apartment.rooms = 3;
+  
+//   apartment.location = {}
+//   apartment.location.city = "Kingston";
+// apartment.location.country = "Peru";
+
+
+//   console.log(apartment);
