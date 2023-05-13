6211b150c4f15cf827e11e9c37340fe8c7663b8a
KostiZin <pancostian@gmail.com>
2023-05-12 22:00:20 -0400

    Update test4.js

--- test4.js
+++ test4.js
@@ -304,22 +304,22 @@
 
 // TASK ==========================================
 
-const atTheOldToad = {
-	potions: [
-		{ name: "Speed potion", price: 460 },
-		{ name: "Dragon breath", price: 780 },
-		{ name: "Stone skin", price: 520 },
-	],
+// const atTheOldToad = {
+// 	potions: [
+// 		{ name: "Speed potion", price: 460 },
+// 		{ name: "Dragon breath", price: 780 },
+// 		{ name: "Stone skin", price: 520 },
+// 	],
 
-	// 	addPotion(newPotion) {
-	// 		for (let potion of this.potions) {
-	// 						if (potion.name === newPotion.name) {
-	// 				return "Error! Potion " + newPotion.name + " is already in your inventory!";
-	// 			}
-	// 		}
-	// 		this.potions.push(newPotion);
-	// 	},
-	// };
+// 	addPotion(newPotion) {
+// 		for (let potion of this.potions) {
+// 						if (potion.name === newPotion.name) {
+// 				return "Error! Potion " + newPotion.name + " is already in your inventory!";
+// 			}
+// 		}
+// 		this.potions.push(newPotion);
+// 	},
+// };
 
 // 	removePotion(potionName) {
 // 		for (let potion of this.potions.name) {
